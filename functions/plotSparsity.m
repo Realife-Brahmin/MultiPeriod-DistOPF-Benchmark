896eefb3ef52194cc2b289ae830355eba25e5e38
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-09-19 13:10:24 -0700

    plotSparsity now syncs rows of Aeq and beq.

--- functions/plotSparsity.m
+++ functions/plotSparsity.m
@@ -12,23 +12,26 @@ function plotSparsity(Aeq, beq)
     [iAeq, jAeq] = find(Aeq);
     [ibeq, ~] = find(beq);
 
+    % Determine positions for the axes objects
+    axWidthAeq = 0.6;
+    axWidthBeq = 0.2;
+    axHeight = size(Aeq, 1) / (size(Aeq, 1) + size(beq, 1));
+    axPosAeq = [0.1, 0.1, axWidthAeq, axHeight];
+    axPosBeq = [0.8, 0.1, axWidthBeq, axHeight];
+
     % Plot Aeq using scatter with wine red color and adjusted marker size
-    subplot(1, 2, 1);
+    ax1 = axes('Position', axPosAeq);
     scatter(jAeq, -iAeq, 20, [0.5, 0, 0.5], 'filled');  % wine red color
     title('Sparsity pattern of Aeq', 'FontWeight', 'bold', 'FontSize', 14);
-    axis equal;
-    xlim([0, size(Aeq, 2)]);
-    ylim([-size(Aeq, 1), 0]);
+    axis equal tight;
     grid on;
     set(gca, 'FontSize', 12, 'FontWeight', 'bold');
 
     % Plot beq using scatter with dark green color
-    subplot(1, 2, 2);
+    ax2 = axes('Position', axPosBeq);
     scatter(ones(size(ibeq)), -ibeq, 20, [0, 0.5, 0], 'filled');  % dark green color
     title('Sparsity pattern of beq', 'FontWeight', 'bold', 'FontSize', 14);
-    axis equal;
-    xlim([0, 2]);  % just a bit of padding for visualization
-    ylim([-size(Aeq, 1), 0]);
+    axis equal tight;
     grid on;
     set(gca, 'FontSize', 12, 'FontWeight', 'bold');
 
