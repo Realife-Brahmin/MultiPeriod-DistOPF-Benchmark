1138e4a9486f17f92a03f2f4d2053579648c625b
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-10-17 13:19:25 -0700

    NOW, there are no data structure errors for T =  1 and 2

--- functions/getIndicesT.m
+++ functions/getIndicesT.m
@@ -1,19 +1,3 @@
-% function result = getIndicesT(cellArray, index)
-%     %extractFromCells Extracts the i-th element from each cell of the input cell array.
-%     %
-%     % Usage:
-%     %   result = getIndicesT(cellArray, index)
-%     %
-%     % Input:
-%     %   cellArray: A 1xN cell array where each cell contains an array of numbers.
-%     %   index: The position of the element to extract from each array inside the cells.
-%     %
-%     % Output:
-%     %   result: A 1xN array containing the extracted elements.
-%     %
-% 
-%     result = cellfun(@(x) x(index), cellArray);
-% end
 function result = getIndicesT(matrix2D, col)
     %GETINDICEST Extracts the col-th element from each row of the input 2D array.
     %
