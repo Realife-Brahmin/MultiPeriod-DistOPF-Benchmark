9dc46e9b8dc41ba24361fa2086bea2a4f7554076
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-10-27 16:22:42 -0700

    Test added for testing linDS
    They converge for macroItr 1, that means initialization of linDS is good.

--- functions/OneTimeScripts/compareLinearArrays.jl
+++ functions/OneTimeScripts/compareLinearArrays.jl
@@ -1,14 +1,17 @@
-include("plotSparsity.jl")
+# include("plotSparsity.jl")
 include("plot_sparsity_pattern.jl")
 using CSV
 using DataFrames
+using Plots
+using Test
 
 # Define your variables
 numAreas = 4
-Area = 2
+@show Area = rand(1:4)
 systemName = "ieee123"
 T = 1
-macroItr = 1
+# @show macroItr = rand(1:6)
+@show macroItr = 1 
 
 # Construct the relative file paths
 baseDirB = joinpath("processedData", "$(systemName)", "numAreas_$(numAreas)", "area$(Area)")
@@ -44,3 +47,14 @@ filePathAeqA_in_B = joinpath(baseDirB, "Aeq_A_T_$(T)_macroItr_$(macroItr)"*ext)
 
 plot_sparsity_pattern(AeqA, beqA, savePlots=true, filenames=[filePathAeqA_in_A, filePathAeqA_in_B])
 
+try @test AeqA ≈ AeqB atol = 1e-5
+catch
+    println("Aeq not equal.")
+    println(maximum(abs.(AeqA-AeqB)))
+end
+
+try @test beqA ≈ beqB atol = 1e-5
+catch
+    println("beq not equal.")
+    println(maximum(abs.(beqA-beqB)))
+end
