e8c8f506acbd4d65322925b1c9a13204cd7a8ef9
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-11-21 11:17:14 -0800

    Stuff printed out better. Need to figure out the deal with weird substation power and where are the voltages.

--- functions/OneTimeScripts/openDSSTest.jl
+++ functions/OneTimeScripts/openDSSTest.jl
@@ -17,24 +17,30 @@ dss("""
     solve
 """)
 
+factor_W_to_kW = 1e-3
+
 solution = Solution.Solve();
-losses = Circuit.Losses()
-S_Substation = Circuit.TotalPower()
+losses_kVAr = Circuit.Losses()*factor_W_to_kW
+S_Substation_kVAr = Circuit.TotalPower()*factor_W_to_kW
+PSubs = real(S_Substation_kVAr)
+QSubs = imag(S_Substation_kVAr)
+PLoss = real(losses_kVAr)
+QLoss = imag(losses_kVAr)
 
 loadnumber = Loads.First()
 kWsum = 0.0
 kvarsum = 0.0
 
-while loadnumber > 0
+while loadnumber > 0 # Loads.Next() returns 0 to signify that all loads have been taken into account
     global kWsum, kvarsum, loadnumber
     kWsum += Loads.kW()
     kvarsum += Loads.kvar()
     loadnumber = Loads.Next()
 end
 
-println("P_L = $kWsum")
-println("Q_L = $(kvarsum)")
-println("PLoss = $(real(losses))")
-println("QLoss = $(imag(losses))")
-println("PSubs = $(real(S_Substation))")
-println("QSubs = $(imag(S_Substation))")
+println("P_L = $(kWsum) kW")
+println("Q_L = $(kvarsum) kVAr")
+println("PLoss = $(PLoss) kW")
+println("QLoss = $(QLoss) kVAr")
+println("PSubs = $(PSubs) kW")
+println("QSubs = $(QSubs) kVAr")
