380782f1af5ac025ad013667f3fb3b5f9e98ebab
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-11-21 11:02:54 -0800

    I'm able to make API calls effortlessly, its just that they don't really make much sense rn

--- functions/OneTimeScripts/openDSSTest.jl
+++ functions/OneTimeScripts/openDSSTest.jl
@@ -5,14 +5,10 @@ using Test
 
 # Store the current directory
 file_directory = @__DIR__ # directory of this .jl file
-wd = dirname(dirname(file_directory)) # goes to the root directory of my workspace
-println(wd)
 
 foldername = joinpath(".", "functions", "OneTimeScripts", "OpenDSSDirect", "5wwHs", "examples", "IEEE123-SinglePhase")
 
 filename = joinpath(foldername, "prev_123JPT.dss")
-
-filename_csv = joinpath(foldername, csvfilename)
 
 
 dss("""
@@ -21,21 +17,24 @@ dss("""
     solve
 """)
 
-function main()
-    loadnumber = Loads.First()
-    kWsum = 0.0
-    kvarsum = 0.0
-    
-    while loadnumber > 0
-        kWsum += Loads.kW()
-        kvarsum += Loads.kvar()
-        loadnumber = Loads.Next()
-    end
+solution = Solution.Solve();
+losses = Circuit.Losses()
+S_Substation = Circuit.TotalPower()
 
-    kWsum, kvarsum
+loadnumber = Loads.First()
+kWsum = 0.0
+kvarsum = 0.0
+
+while loadnumber > 0
+    global kWsum, kvarsum, loadnumber
+    kWsum += Loads.kW()
+    kvarsum += Loads.kvar()
+    loadnumber = Loads.Next()
 end
 
-result = main()
-
-
-# println(result)
+println("P_L = $kWsum")
+println("Q_L = $(kvarsum)")
+println("PLoss = $(real(losses))")
+println("QLoss = $(imag(losses))")
+println("PSubs = $(real(S_Substation))")
+println("QSubs = $(imag(S_Substation))")
