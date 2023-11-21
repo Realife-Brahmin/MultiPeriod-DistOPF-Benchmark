cf8a13853c4cae1ad714e0aa01862c156f0b80d3
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-11-21 10:39:21 -0800

    wait what did I save the previous commit?
    Okay <now> 123 is tested.

--- functions/OneTimeScripts/openDSSTest.jl
+++ functions/OneTimeScripts/openDSSTest.jl
@@ -7,22 +7,13 @@ using Test
 file_directory = @__DIR__ # directory of this .jl file
 wd = dirname(dirname(file_directory)) # goes to the root directory of my workspace
 println(wd)
-csvfilename = "Normalized-1s-2900-pts.csv"
 
-# foldername = joinpath(".", "functions", "OneTimeScripts", "OpenDSSDirect", "5wwHs", "examples", "8500-Node")
+foldername = joinpath(".", "functions", "OneTimeScripts", "OpenDSSDirect", "5wwHs", "examples", "IEEE123-SinglePhase")
 
-foldername = joinpath(".", "functions", "OneTimeScripts", "OpenDSSDirect", "5wwHs", "examples", "8500-Node with some spaces in the name") # notice in the last folder that I've created a duplicate of the 8500-Node folder but inserted spaces in its name
-
-filename = joinpath(foldername, "Master.dss")
-
-# filename = joinpath(dirname(dirname(pathof(OpenDSSDirect))), "examples", "8500-Node", "Master.dss")
+filename = joinpath(foldername, "prev_123JPT.dss")
 
 filename_csv = joinpath(foldername, csvfilename)
 
-# println(filename_csv)
-
-# df = CSV.File(filename_csv, header=0) |> DataFrame; # This works for both filenames, just to check that the folder with spaces in its name exists
-# display(df)
 
 dss("""
     clear
@@ -46,8 +37,5 @@ end
 
 result = main()
 
-@test lowercase(pwd()) == lowercase(wd)
-# Reset the working directory to its original state
-# cd(original_directory)
 
 # println(result)
