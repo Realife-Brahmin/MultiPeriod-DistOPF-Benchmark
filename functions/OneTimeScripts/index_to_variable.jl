1ec0291d4badc9d906ccc3e131446a7a40e1546f
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-11-29 15:21:10 -0800

    rmunloc

--- functions/OneTimeScripts/index_to_variable.jl
+++ functions/OneTimeScripts/index_to_variable.jl
@@ -7,19 +7,14 @@ function index_to_variable(indices, areaInfo)
     for varNum = 1:numVars
         index = indices[varNum]
         if index <= m_Area
-            # return "Pij($(index))"
             notations[varNum] = "Pij($(index))";
         elseif index <= 2 * m_Area
-            # return "Qij($(index - m_Area))"
             notations[varNum] = "Qij($(index - m_Area))";
         elseif index <= 3 * m_Area
-            # return "lij($(index - 2 * m_Area))"
             notations[varNum] = "lij($(index - 2 * m_Area))";
         elseif index <= 3 * m_Area + N_Area
-            # return "vj($(index - 3 * m_Area))"
             notations[varNum] = "vj($(index - 3 * m_Area))";
         else
-            # return "qD($(index - 3 * m_Area - N_Area))"
             notations[varNum] = "qD($(index - 3 * m_Area - N_Area))"
         end
     end
@@ -27,11 +22,11 @@ function index_to_variable(indices, areaInfo)
     return notations
 end
 
-# Example usage:
-m_Area = 127
-N_Area = 128
-nD_Area = 85
-areaInfo = Dict(:m_Area => m_Area, :N_Area => N_Area, :nD_Area => nD_Area)
-index = [21, 275]  # Replace with the actual index
-variable_representation = index_to_variable(index, areaInfo)
-println(variable_representation)
+# # Example usage:
+# m_Area = 127
+# N_Area = 128
+# nD_Area = 85
+# areaInfo = Dict(:m_Area => m_Area, :N_Area => N_Area, :nD_Area => nD_Area)
+# index = [21, 275]  # Replace with the actual index
+# variable_representation = index_to_variable(index, areaInfo)
+# println(variable_representation)
