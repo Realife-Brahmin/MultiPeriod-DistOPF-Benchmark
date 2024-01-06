0b6f378dc9d1a875ea38358be79336c8b5896c0f
= <aryan.r.jha@gmail.com>
2024-01-05 21:57:17 -0800

    plotting implementation started

---
+++ functions/OneTimeScripts/validationAgainstOpenDSS_MATLAB/ieee123_1ph_created/scripts/plotVoltages.jl
@@ -0,0 +1,10 @@
+# currently under implementation
+
+pv = 10
+pvString = 
+batt = 10
+hour = 1
+resultsFolder = "../results/"
+configFolder = resultsFolder*"pv"*number_to_padded_string(pv)*"_batt"*number_to_padded_string(batt)*"/"
+
+
