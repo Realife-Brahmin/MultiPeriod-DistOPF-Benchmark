900832c0314e0ba8eed11f05bb46bc6e9b87a5b2
= <aryan.r.jha@gmail.com>
2024-01-06 22:45:26 -0800

    votlages and busnames imported into the workspace

--- functions/OneTimeScripts/validationAgainstOpenDSS_MATLAB/ieee123_1ph_created/scripts/plotVoltages.jl
+++ functions/OneTimeScripts/validationAgainstOpenDSS_MATLAB/ieee123_1ph_created/scripts/plotVoltages.jl
@@ -1,10 +1,23 @@
 # currently under implementation
+using CSV
+using DataFrames
+include("number_to_padded_string.jl")
 
 pv = 10
 pvString = 
 batt = 10
 hour = 1
-resultsFolder = "../results/"
-configFolder = resultsFolder*"pv"*number_to_padded_string(pv)*"_batt"*number_to_padded_string(batt)*"/"
+wd = @__DIR__
 
+resultsFolder = joinpath(dirname(wd), "results")
+configFolderName = "pv"*number_to_padded_string(pv)*"_batt"*number_to_padded_string(batt)
+configFolder = joinpath(resultsFolder, configFolderName)
+ext = ".csv"
 
+filenameVoltage = "voltages"*string(hour)*ext
+filename = joinpath(configFolder, filenameVoltage)
+
+dfVoltage = CSV.read(filename, DataFrame)
+
+filenameBusNames = joinpath(wd, "busname.txt")
+busNames = CSV.read(filenameBusNames, DataFrame, header=false)
