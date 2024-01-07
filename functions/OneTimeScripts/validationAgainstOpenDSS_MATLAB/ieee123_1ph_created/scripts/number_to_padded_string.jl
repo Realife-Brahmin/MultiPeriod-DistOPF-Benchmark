d02482c084965297b362fa72e47a9862b7bd2826
= <aryan.r.jha@gmail.com>
2024-01-06 22:45:05 -0800

    cmunlocs

--- functions/OneTimeScripts/validationAgainstOpenDSS_MATLAB/ieee123_1ph_created/scripts/number_to_padded_string.jl
+++ functions/OneTimeScripts/validationAgainstOpenDSS_MATLAB/ieee123_1ph_created/scripts/number_to_padded_string.jl
@@ -4,7 +4,7 @@ function number_to_padded_string(num::Int)
     return @sprintf("%03d", num)
 end
 
-# Example usage:
-println(number_to_padded_string(100))  # Outputs: "100"
-println(number_to_padded_string(5))    # Outputs: "005"
-println(number_to_padded_string(10))   # Outputs: "010"
+# # Example usage:
+# println(number_to_padded_string(100))  # Outputs: "100"
+# println(number_to_padded_string(5))    # Outputs: "005"
+# println(number_to_padded_string(10))   # Outputs: "010"
