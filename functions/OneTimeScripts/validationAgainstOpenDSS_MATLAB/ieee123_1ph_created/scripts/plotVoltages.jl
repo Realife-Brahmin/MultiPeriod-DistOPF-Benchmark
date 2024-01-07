83bb045fa49fed8b8e68bef729c674cb8c832fa1
= <aryan.r.jha@gmail.com>
2024-01-07 09:02:55 -0800

    summary (the last run) for hour-th hour added to workspace as a df

--- functions/OneTimeScripts/validationAgainstOpenDSS_MATLAB/ieee123_1ph_created/scripts/plotVoltages.jl
+++ functions/OneTimeScripts/validationAgainstOpenDSS_MATLAB/ieee123_1ph_created/scripts/plotVoltages.jl
@@ -14,10 +14,17 @@ configFolderName = "pv"*number_to_padded_string(pv)*"_batt"*number_to_padded_str
 configFolder = joinpath(resultsFolder, configFolderName)
 ext = ".csv"
 
+filenameSummary = "summary"*string(hour)*ext
 filenameVoltage = "voltages"*string(hour)*ext
-filename = joinpath(configFolder, filenameVoltage)
-
-dfVoltage = CSV.read(filename, DataFrame)
-
+filenameVoltageFull = joinpath(configFolder, filenameVoltage)
+filenameSummaryFull = joinpath(configFolder, filenameSummary)
+dfVoltage = CSV.read(filenameVoltageFull, DataFrame)
+dfSummary = CSV.read(filenameSummaryFull, DataFrame)
+dfSummary = dfSummary[end, :]
 filenameBusNames = joinpath(wd, "busname.txt")
 busNames = CSV.read(filenameBusNames, DataFrame, header=false)
+
+rename!(dfVoltage, [:Bus, :BasekV, :Node1, :Magnitude1, :Angle1, :pu1]) # rename headers when I come back, remove the leading space
+
+V_unsorted = dfVoltage.pu1
+busNames
