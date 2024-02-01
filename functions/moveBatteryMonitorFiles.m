f00ee9d02a86a12ecfc78581cf65218370dc99a8
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-01-31 16:45:11 -0800

    verbose kwarg added to the monitor csv moving function

--- functions/moveBatteryMonitorFiles.m
+++ functions/moveBatteryMonitorFiles.m
@@ -1,4 +1,19 @@
-function moveBatteryMonitorFiles(circuitName, busesWithBatts_Area, nBatt_Area, batteryMonitorFolder)
+function moveBatteryMonitorFiles(circuitName, busesWithBatts_Area, nBatt_Area, batteryMonitorFolder, varargin)
+    % Create an input parser
+    p = inputParser;
+    
+    % Define default value for verbose
+    defaultVerbose = false;
+    
+    % Add the verbose parameter to the parser
+    addParameter(p, 'verbose', defaultVerbose, @islogical);
+    
+    % Parse the input arguments
+    parse(p, varargin{:});
+    
+    % Retrieve the verbose value
+    verbose = p.Results.verbose;
+
     % Iterating through each bus with a battery
     for i = 1:nBatt_Area
         busNum = busesWithBatts_Area(i);
@@ -12,9 +27,15 @@ function moveBatteryMonitorFiles(circuitName, busesWithBatts_Area, nBatt_Area, b
 
             % Move the file to the new location
             movefile(csvFileName, newFilePath);
-            disp(['Moved: ', csvFileName, ' to ', newFilePath]);
+            
+            % Display information if verbose is true
+            if verbose
+                disp(['Moved: ', csvFileName, ' to ', newFilePath]);
+            end
         else
-            disp(['File not found: ', csvFileName]);
+            if verbose
+                disp(['File not found: ', csvFileName]);
+            end
         end
     end
 end
