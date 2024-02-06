d96f6de8ffa3f13a13ba82f7e01911fd32745a9c
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-02-05 16:36:40 -0800

    rmunlocs

--- functions/generateRangesFromValuesT.m
+++ functions/generateRangesFromValuesT.m
@@ -1,33 +1,3 @@
-% function outputArrays = generateRangesFromValuesT(values, T)
-%     numVarsPerTimePeriod = sum(values);  % Total number of variables in one time step
-%     indexRangeBorders = cumsum([0, values]);
-%     numArrays = numel(indexRangeBorders) - 1;
-%     outputArrays = cell(1, numArrays);
-% 
-%     for i = 1:numArrays
-%         tempRanges = cell(1, T);
-%         for t = 1:T
-%             offset = (t-1) * numVarsPerTimePeriod;
-%             tempRanges{t} = (indexRangeBorders(i)+1 + offset):(indexRangeBorders(i+1) + offset);
-%         end
-%         outputArrays{i} = tempRanges;
-%     end
-% end
-% function outputArrays = generateRangesFromValuesT(values, T)
-%     numVarsPerTimePeriod = sum(values);  % Total number of variables in one time step
-%     indexRangeBorders = cumsum([0, values]);
-%     numArrays = numel(indexRangeBorders) - 1;
-%     outputArrays = cell(1, numArrays);
-% 
-%     for i = 1:numArrays
-%         tempRanges = zeros(T, diff(indexRangeBorders(i:i+1)));
-%         for t = 1:T
-%             offset = (t-1) * numVarsPerTimePeriod;
-%             tempRanges(t, :) = (indexRangeBorders(i)+1 + offset):(indexRangeBorders(i+1) + offset);
-%         end
-%         outputArrays{i} = tempRanges;
-%     end
-% end
 function outputArrays = generateRangesFromValuesT(values, T)
     numVarsPerTimePeriod = sum(values);  % Total number of variables in one time step
     indexRangeBorders = cumsum([0, values]);
