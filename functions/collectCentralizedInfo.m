fd725285d30a1c4aa34c43d47bfe7968c7688945
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-02-23 20:17:45 -0800

    still recognizing what should go into `sysInfo` and how to do so.

--- functions/collectCentralizedInfo.m
+++ functions/collectCentralizedInfo.m
@@ -17,5 +17,13 @@ function sysInfo = collectCentralizedInfo(sysInfo, simInfo)
         sysInfo.P_L_1toT(bus1, 1:T) = areaInfo.P_L_Area_1toT;
         sysInfo.Q_L_1toT(bus1, 1:T) = areaInfo.Q_L_Area_1toT;
         
+        sysInfo.Q_C_Full(bus1) = areaInfo.Q_C_Area
+        sysInfo.Pmpp_Full(bus1) = areaInfo.Pmpp_Area
+
+        sysInfo.pD_Full_1toT(bus1, 1:T) = areaInfo.P_der_Area_1toT;
+        % figure out how to create sysInfo.pD_1toT with only nDER buses
+
+        % figure out how to create busesWithDERSonly for sysInfo
+
     end
 end
