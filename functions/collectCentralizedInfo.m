1d787fc00890c6b25b0399342c3803201e748162
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-02-24 21:47:56 -0800

    component values locs defined (indexing numbers themselves need to be retrieved/created first)

--- functions/collectCentralizedInfo.m
+++ functions/collectCentralizedInfo.m
@@ -18,28 +18,37 @@ function sysInfo = collectCentralizedInfo(sysInfo, simInfo)
         sysInfo.Q_L_1toT(bus1, 1:T) = areaInfo.Q_L_Area_1toT;
         
         sysInfo.Q_C_Full(bus1) = areaInfo.Q_C_Area
-        sysInfo.Pmpp_Full(bus1) = areaInfo.Pmpp_Area
+        % sysInfo.Pmpp_Full(bus1) = areaInfo.Pmpp_Area
 
-        sysInfo.pD_Full_1toT(bus1, 1:T) = areaInfo.P_der_Area_1toT;
+        % sysInfo.pD_Full_1toT(bus1, 1:T) = areaInfo.P_der_Area_1toT;
         % figure out how to create sysInfo.pD_1toT with only nDER buses
 
         % figure out how to create busesWithDERSonly for sysInfo
         
-        bus1_DER = areaInfo.bus1_Actual_DER; % this gives me the actual bus number where the DER is placed.
+        % bus1_DER = areaInfo.bus1_Actual_DER; % this gives me the actual bus number where the DER is placed.
         % This is not useful for non-Full variables for sysInfo for me.
         % I somehow need to get the batt_num_sys values
         
+        % numDERBus will be like [1 7 22 85] which will only contain values
+        % between 1:n_DER_System, so no actual bus numbers
+        sysInfo.Sder(numDERBus) = areaInfo.S_der_Area;
+        sysInfo.Pmpp(numDERBus) = areaInfo.Pmpp_Area;
+        sysInfo.pD_1toT(numDERBus, 1:T) = areaInfo.P_der_Area_1toT;
         sysInfo.qD_1toT(numDERBus, 1:T) = areaInfo.qD_Area_1toT;
+        
+        % numBattBus will be like [1 7 22 85] which will only contain values
+        % between 1:n_DER_System, so no actual bus numbers
+        sysInfo.Sbatt(numBattBus) = areaInfo.S_battMax_Area;
         sysInfo.B0(numBattBus) = areaInfo.B0Vals_pu_Area; 
         sysInfo.B_1toT(numBattBus, 1:T) = areaInfo.B_Area_1toT;
         sysInfo.Pd_1toT(numBattBus, 1:T) = areaInfo.Pd_Area_1toT;
-
-        sysInfo.Pc_1toT(battBusNum, 1:T) = areaInfo.Pc_Area_1toT;
-
-        sysInfo.qB_1toT(bus1_Batt, 1:T) = areaInfo.qB_Area_1toT;
+        sysInfo.Pc_1toT(numBattBus, 1:T) = areaInfo.Pc_Area_1toT;
+        sysInfo.qB_1toT(numBattBus, 1:T) = areaInfo.qB_Area_1toT;
     
         
 
     end
+    
+    % loadShapePV = simInfo.pvCoeffVals;
 
 end
