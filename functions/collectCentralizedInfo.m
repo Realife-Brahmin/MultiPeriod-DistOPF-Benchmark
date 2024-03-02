eb27ca87021f45951fff56168b42bfac1210fecd
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-03-01 20:22:16 -0800

    m

--- functions/collectCentralizedInfo.m
+++ functions/collectCentralizedInfo.m
@@ -11,27 +11,20 @@ function sysInfo = collectCentralizedInfo(sysInfo, simInfo)
         
         error("Have you inserted actual bus1, actual fb and actual tb values for the area?")
         
+        % busData, branchData
         bus1 = areaInfo.bus1_Actual;
-        % fb_Area = areaInfo.fb_Area;
         fb = areaInfo.fb_Actual;
-        % tb_Area = areaInfo.tb_Area;
         tb = areaInfo.tb_Actual;
-        % P_L_1toT = sysInfo.P_L_1toT;
         sysInfo.P_L_1toT(bus1, 1:T) = areaInfo.P_L_Area_1toT;
         sysInfo.Q_L_1toT(bus1, 1:T) = areaInfo.Q_L_Area_1toT;
         sysInfo.V_1toT(bus1, 1:T) = areaInfo.V_Area_1toT;
         sysInfo.Q_C_Full(bus1) = areaInfo.Q_C_Area;
-        % sysInfo.Pmpp_Full(bus1) = areaInfo.Pmpp_Area
 
-        % sysInfo.pD_Full_1toT(bus1, 1:T) = areaInfo.P_der_Area_1toT;
         % figure out how to create sysInfo.pD_1toT with only nDER buses
 
         % figure out how to create busesWithDERSonly for sysInfo
         
-        % bus1_DER = areaInfo.bus1_Actual_DER; % this gives me the actual bus number where the DER is placed.
-        % This is not useful for non-Full variables for sysInfo for me.
-        % I somehow need to get the batt_num_sys values
-        
+        % DER Parameters
         % numDERBus will be like [1 7 22 85] which will only contain values
         % between 1:n_DER_System, so no actual bus numbers
         sysInfo.Sder(numDERBus) = areaInfo.S_der_Area;
@@ -39,6 +32,8 @@ function sysInfo = collectCentralizedInfo(sysInfo, simInfo)
         sysInfo.pD_1toT(numDERBus, 1:T) = areaInfo.P_der_Area_1toT;
         sysInfo.qD_1toT(numDERBus, 1:T) = areaInfo.qD_Area_1toT;
         
+
+        % Battery Parameters
         % numBattBus will be like [1 7 22 85] which will only contain values
         % between 1:n_DER_System, so no actual bus numbers
         sysInfo.Sbatt(numBattBus) = areaInfo.S_battMax_Area;
