7892f1f86d0bfddcc8a1ea4cc016a08667010a17
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-04-09 18:21:14 -0700

    Now loc numbers in results will be printed as 01, 02 and so on.

--- functions/print_and_write.m
+++ functions/print_and_write.m
@@ -1,5 +1,6 @@
 % Define a helper function for simultaneous console and file output
 function print_and_write(fileID, locNum, formatSpec, varargin)
-    fprintf(1, [num2str(locNum), '. ', formatSpec], varargin{:}); % Print to Command Window
-    fprintf(fileID, [num2str(locNum), '. ', formatSpec], varargin{:}); % Write to file
+    locNumStr = sprintf('%02d', locNum);  % Use sprintf to format the number with leading zeros
+    fprintf(1, [locNumStr, '. ', formatSpec], varargin{:}); % Print to Command Window
+    fprintf(fileID, [locNumStr, '. ', formatSpec], varargin{:}); % Write to file
 end
