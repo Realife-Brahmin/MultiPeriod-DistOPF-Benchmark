e2e26adccfbe7aa33a001741f3de6367cd7848bf
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-04-15 18:36:19 -0700

    Currently testing with generated profiles instead of the OpenDSS ones

--- functions/inputForecastData.m
+++ functions/inputForecastData.m
@@ -6,7 +6,7 @@ function [pvCoeffVals, lambdaVals, S_to_P_ratio_PV, S_to_P_ratio_Batt, costArray
 
     % Define the file extension
     ext = ".csv";
-
+    
     % Construct file paths
     LoadShapePV24_addr = strcat(rawDataFolder, filesep, "LoadShapePV24", ext);
     LoadShape24_addr = strcat(rawDataFolder, filesep, "LoadShape24", ext);
@@ -20,9 +20,11 @@ function [pvCoeffVals, lambdaVals, S_to_P_ratio_PV, S_to_P_ratio_Batt, costArray
     LoadShapePV24 = table2array(LoadShapePV24_table(:, 2));
 
     % Apply global PV Coefficient and choose middle T points
-    pvCoeffVals = globalPVCoeff * choose_middle_T_points(LoadShapePV24, T);
-    lambdaVals = choose_middle_T_points(LoadShape24, T);
+    % pvCoeffVals = globalPVCoeff * choose_middle_T_points(LoadShapePV24, T);
+    pvCoeffVals = transpose(globalPVCoeff * generatePVProfile(T, 0.6, 1.4, 0.8));
 
+    % lambdaVals = choose_middle_T_points(LoadShape24, T);
+    lambdaVals = transpose(generateLoadProfile(T, 0.7, 1.0, 'normal'));
     % Calculate S_to_P_ratio for PV and Battery
     S_to_P_ratio_PV = 1.2 * (min(1, max(pvCoeffVals)));
     S_to_P_ratio_Batt = S_to_P_ratio_PV;
@@ -30,5 +32,5 @@ function [pvCoeffVals, lambdaVals, S_to_P_ratio_PV, S_to_P_ratio_Batt, costArray
     % Sample cost array and choosing middle T points
     costArray0 = [1.6, 3.8, 1.7, 1.8, 2.1, 2.2, 2.4, 2.9, 4.4, 5.1, 4.3, 3.5, 2.8, 2.8, 2.3, 2.0, 2.3, 4.1, 3.4, 6.6, 4.7, 4.7, 5.0, 2.6]';
     costArray = choose_middle_T_points(costArray0, T);
-
+    
 end
